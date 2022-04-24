60e42322a6d7573b72255502ab95a4f6302a5bcd
Chris Percival <percivalcj@gmail.com>
2022-04-24 19:15:16 -0400

    Add second column to NamesView Add TextView and Picker to Names View for selecting noPairs

--- BridgeScores/BridgeScoresApp.swift
+++ BridgeScores/BridgeScoresApp.swift
@@ -11,7 +11,6 @@ import SwiftUI
 struct BridgeScoresApp: App {
     var body: some Scene {
         WindowGroup {
-//            NamesView()
             ContentView()
         }
     }
--- BridgeScores/Views/NamesView.swift
+++ BridgeScores/Views/NamesView.swift
@@ -10,17 +10,34 @@ import SwiftUI
 struct NamesRowView:View {
     var rowNo:Int
     @State var playerIndex = 0
+    @State var playerIndex1 = 0
     @EnvironmentObject var belfastPlayers: BelfastPlayers
     var body: some View{
         HStack {
-            Text(String(rowNo + 1))
+            Text(String(rowNo + 1)).frame(width:30)
             Picker("First Name", selection: $playerIndex, content: {
-                ForEach(0..<belfastPlayers.players.count,id:\.self){ i in
+                ForEach(0..<belfastPlayers.players.count,id:\.self)
+                    { i in
                     Text(belfastPlayers.players[i].firstName + " " +
                          belfastPlayers.players[i].lastName)
+                    .frame(width: 200)
+                    .foregroundColor(.black)
                 }
             })
+            Spacer()
+            Picker("First Name", selection: $playerIndex1, content: {
+                ForEach(0..<belfastPlayers.players.count,id:\.self)
+                    { i in
+                    Text(belfastPlayers.players[i].firstName + " " +
+                         belfastPlayers.players[i].lastName)
+                    .frame(width:200)
+                    .foregroundColor(.black)
+                }
+            })
+            .foregroundColor(.black)
+            Spacer()
         }
+        .frame(width: 400, alignment: .leading)
 
     }
     
@@ -29,20 +46,32 @@ struct NamesRowView:View {
 struct NamesView: View {
     @State var matchPlayers = MatchPlayers()
     @State var belfastPlayers = BelfastPlayers()
-    private var noPairs = 12
+    @State var noPairs = 6
     var body: some View {
-        VStack(alignment:.leading) {
-            ForEach(0..<noPairs,id:\.self) { i in
-                NamesRowView(rowNo: i)
+        VStack {
+            HStack {
+                Text("Number of Pairs:")
+                Picker("Number of Pairs:",selection: $noPairs){
+                    ForEach(1...12,id:\.self){i in
+                        Text(String(i))
+                    }
+                }
             }
+            VStack(alignment:.leading) {
+                ForEach(0..<noPairs,id:\.self) { i in
+                    NamesRowView(rowNo: i)
+                }
+            }
+            .environmentObject(belfastPlayers)
+            .padding()
+        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
         }
-        .environmentObject(belfastPlayers)
     }
 }
 
 struct NamesView_Previews: PreviewProvider {
     static var previews: some View {
         NamesView()
-            .previewInterfaceOrientation(.landscapeLeft)
+            .previewInterfaceOrientation(.portrait)
     }
 }
