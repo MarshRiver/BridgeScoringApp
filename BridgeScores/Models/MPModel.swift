26dcc3b441839bd5c776e0764301436e89fe3f1f
Chris Percival <percivalcj@gmail.com>
2022-05-14 20:15:25 -0400

    Move code from playground to MPModel Not debugged in app

--- BridgeScores/Models/MPModel.swift
+++ BridgeScores/Models/MPModel.swift
@@ -13,7 +13,7 @@ class MatchPointTable {
     let minusInfinity = -Int.max
     var noPairs:Int = 6
     var matchTable = [MatchPointRow]()
-    
+
     init() {
         matchTable = Array(repeating:MatchPointRow(),count:noPairs)
     }
@@ -23,17 +23,58 @@ class MatchPointTable {
         
         //Assign current board contracts to our board array
         var board = match.Boards[boardNo].contracts
+        var MP = Array(repeating: 0.0 , count: board.count)
+        var ties = Array(repeating: 0.0 , count: board.count)
+
         //Remove contracts with no players
         board.removeAll(where: {
             $0.nsPair == ""})
         
         //TODO: Assign negative scores from EW score
-        
+        for index in 0..<board.count {
+            if board[index].nsScore == "" {
+                board[index].nsScore = String(-(Int(board[index].ewScore) ?? minusInfinity))
+            }
+        }
         
         //Sort by NSscore
         board.sort{Int($0.nsScore) ?? minusInfinity > Int($1.nsScore) ?? minusInfinity}
+        //Calculate Ties
+        for outerIndex in 0..<board.count {
+            for innerIndex in 0..<board.count{
+                if board[innerIndex].nsScore == board[outerIndex].nsScore{
+                    ties[outerIndex] += 1.0
+                }
+            }
+        }
+        //Calculate rank
+        var index = 0
+        var prev: Int?              // previous score
+        var currentRank = 0
+        var rankingList = [(name: String, score: Int, rank: Int)]()
         
-        matchTable = Array(repeating: MatchPointRow(), count: noPairs)
+        for contract in board {
+            let nsScoreInt = Int(contract.nsScore) ?? minusInfinity
+            index += 1
+           
+            if prev == nil || nsScoreInt != prev! {
+                prev = nsScoreInt
+                currentRank = index
+            }
+            rankingList.append((contract.nsPair,nsScoreInt,currentRank))
+        }
+        //Caluclate master points
+        index = 0
+        for elem in rankingList {
+            
+            MP[index] = 3.0 - Double(elem.rank) - (ties[index] - 1.0) * 0.5
+            index += 1
+        }
+
+
+        
+        
+//        matchTable = Array(repeating: MatchPointRow(), count: noPairs)
         
         
     }
